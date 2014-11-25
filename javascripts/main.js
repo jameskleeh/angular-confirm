58093384e5cf4117999818ef2d73124a62dfe947
James Kleeh <jjkleeh@ysu.edu>
2014-11-25 13:21:28 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+angular.module('myApp', ['angular-confirm'])
+  .controller('MyController', function($scope, $timeout) {
+    
+    function hide(func) {
+      $timeout(function() {
+        func();
+      }, 3000);
+    }
+    
+    $scope.delete = function() {
+      $scope.deleted = 'Deleted';
+      hide(function() {
+        $scope.deleted = '';
+      })
+    };
+    
+    $scope.deleteTwo = function() {
+      $scope.deleted_two = 'Deleted';
+      hide(function() {
+        $scope.deleted_two = '';
+      })
+    }; 
+    
+  });
