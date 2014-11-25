40d288993fdcb9910613a7fa8a3dce45598ee101
James Kleeh <jjkleeh@ysu.edu>
2014-11-25 14:10:11 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,7 @@ angular.module('myApp', ['angular-confirm'])
   .controller('MyController', function($scope) {
 
     $scope['delete'] = function() {
-      console.log('inside scope delete');
       $scope.deleted = 'Deleted';
-      console.log($scope);
     };
     
   });
