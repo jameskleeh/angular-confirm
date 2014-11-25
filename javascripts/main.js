b5b37b46537f4da6a23f9570549793c9d4548165
James Kleeh <jjkleeh@ysu.edu>
2014-11-25 13:39:35 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,21 +2,24 @@ angular.module('myApp', ['angular-confirm'])
   .controller('MyController', function($scope, $timeout) {
     
     function hide(func) {
-      $timeout(function() {
-        func();
-      }, 3000);
+      return $timeout(func, 3000);
     }
+    
+    var hideTimeout;
+    var hideTimeout_two;
     
     $scope.delete = function() {
       $scope.deleted = 'Deleted';
-      hide(function() {
+      $timeout.cancel(hideTimeout);
+      hideTimeout = hide(function() {
         $scope.deleted = '';
       })
     };
     
     $scope.deleteTwo = function() {
       $scope.deleted_two = 'Deleted';
-      hide(function() {
+      $timeout.cancel(hideTimeout_two);
+      hideTimeout_two = hide(function() {
         $scope.deleted_two = '';
       })
     }; 
