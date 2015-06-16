9259e343b3971831f9e1c879bb21d45b10641263
James Kleeh <jjkleeh@ysu.edu>
2015-06-16 14:24:23 -0400

    Update angular-confirm.js

--- javascripts/angular-confirm.js
+++ javascripts/angular-confirm.js
@@ -1,7 +1,7 @@
 /*
  * angular-confirm
  * http://schlogen.github.io/angular-confirm/
- * Version: 1.0.2 - 2015-28-04
+ * Version: 1.0.4 - 2015-16-06
  * License: Apache
  */
 angular.module('angular-confirm', ['ui.bootstrap'])
@@ -17,56 +17,81 @@ angular.module('angular-confirm', ['ui.bootstrap'])
   };
 }])
 .value('$confirmModalDefaults', {
-  template: '<div class="modal-header"><h3 class="modal-title">Confirm</h3></div><div class="modal-body">{{data.text}}</div><div class="modal-footer"><button class="btn btn-primary" ng-click="ok()">OK</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>',
-  controller: 'ConfirmModalController'
+  template: '<div class="modal-header"><h3 class="modal-title">{{data.title}}</h3></div>' +
+            '<div class="modal-body">{{data.text}}</div>' +
+            '<div class="modal-footer">' +
+            '<button class="btn btn-primary" ng-click="ok()">{{data.ok}}</button>' +
+            '<button class="btn btn-default" ng-click="cancel()">{{data.cancel}}</button>' +
+            '</div>',
+  controller: 'ConfirmModalController',
+  defaultLabels: {
+    title :'Confirm',
+    ok: 'OK',
+    cancel: 'Cancel'
+  }
 })
 .factory('$confirm', ['$modal', '$confirmModalDefaults', function($modal, $confirmModalDefaults) {
   return function(data, settings) {
     settings = angular.extend($confirmModalDefaults, (settings || {}));
-    data = data || {};
-    
+
+    data = angular.extend({}, settings.defaultLabels, data || {});
+	
     if ('templateUrl' in settings && 'template' in settings) {
       delete settings.template;
     }
-    
+
     settings.resolve = {data: function() { return data; }};
 
     return $modal.open(settings).result;
   };
 }])
 .directive('confirm', ['$confirm', function($confirm) {
-    return {
-      priority: 1,
-      restrict: 'A',
-      scope: {
-        confirmIf: "=",
-        ngClick: '&',
-        confirm: '@'
-      },
-      link: function(scope, element, attrs) {
-        function reBind(func) {
-          element.unbind("click").bind("click", function() {
-            func();
-          });
-        }
-        
-        function bindConfirm() {
-          $confirm({text: scope.confirm}).then(scope.ngClick);
-        }
-        
-        if ('confirmIf' in attrs) {
-          scope.$watch('confirmIf', function(newVal) {
-            if (newVal) {
-              reBind(bindConfirm);
-           } else {
-              reBind(function() {
-             	  scope.$apply(scope.ngClick);
-              }); 
-            }
-          });
-        } else {
-          reBind(bindConfirm);
-        }
+  return {
+    priority: 1,
+    restrict: 'A',
+    scope: {
+      confirmIf: "=",
+      ngClick: '&',
+      confirm: '@',
+      confirmTitle: '@',
+      confirmOk: '@',
+      confirmCancel: '@'
+    },
+    link: function(scope, element, attrs) {
+      function reBind(func) {
+        element.unbind("click").bind("click", function($event) {
+          $event.preventDefault();
+          func();
+        });
+      }
+
+      function bindConfirm() {
+        var data = { text: scope.confirm };
+		if (scope.confirmTitle) {
+			data.title = scope.confirmTitle;
+		}
+		if (scope.confirmOk) {
+			data.ok = scope.confirmOk;
+		}
+		if (scope.confirmCancel) {
+			data.cancel = scope.confirmCancel;
+		}
+        $confirm(data).then(scope.ngClick);
+      }
+
+      if ('confirmIf' in attrs) {
+        scope.$watch('confirmIf', function(newVal) {
+          if (newVal) {
+            reBind(bindConfirm);
+          } else {
+            reBind(function() {
+              scope.$apply(scope.ngClick);
+            });
+          }
+        });
+      } else {
+        reBind(bindConfirm);
       }
     }
+  }
 }]);
