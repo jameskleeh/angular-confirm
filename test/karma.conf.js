2cbea400bb66316494f596bc81d388ce378d8363
James Kleeh <jjkleeh@ysu.edu>
2015-10-26 13:07:00 -0400

    Update test config for travis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Tue Jul 14 2015 10:10:58 GMT-0400 (Eastern Daylight Time)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -61,9 +61,21 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+	customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 }
