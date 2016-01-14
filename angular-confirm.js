b95931c45e6ac43d1d8c503d767e90de8dcbc4a0
JC Ford <johncford@gmail.com>
2016-01-13 17:40:09 -0700

    Update angular-confirm.js
    Supporting customized response messages allows custom templates to offer more than the standard 'ok' and 'cancel' buttons.

--- angular-confirm.js
+++ angular-confirm.js
@@ -24,12 +24,13 @@ angular.module('angular-confirm', ['ui.bootstrap.modal'])
   .controller('ConfirmModalController', function ($scope, $uibModalInstance, data) {
     $scope.data = angular.copy(data);
 
-    $scope.ok = function () {
-      $uibModalInstance.close();
+    $scope.ok = function (closeMessage) {
+      $uibModalInstance.close(closeMessage);
     };
 
-    $scope.cancel = function () {
-      $uibModalInstance.dismiss('cancel');
+    $scope.cancel = function (dismissMessage) {
+      if (typeof dismissMessage === 'undefined') dismissMessage = 'cancel';
+      $uibModalInstance.dismiss(dismissMessage);
     };
 
   })
