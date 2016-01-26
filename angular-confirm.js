d458be429fb7695059057f29745701cc86bc1cee
James Kleeh <james.kleeh@gmail.com>
2016-01-26 11:03:18 -0500

    Release 1.2.3

--- angular-confirm.js
+++ angular-confirm.js
@@ -1,22 +1,16 @@
 /*
  * angular-confirm
  * https://github.com/Schlogen/angular-confirm
- * @version v1.2.2 - 2015-12-08
+ * @version v1.2.3 - 2016-01-26
  * @license Apache
  */
 (function (root, factory) {
   'use strict';
   if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
     define(['angular'], factory);
   } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
-    // CommonJS support (for us webpack/browserify/ComponentJS folks)
     module.exports = factory(require('angular'));
   } else {
-    // in the case of no module loading system
-    // then don't worry about creating a global
-    // variable like you would in normal UMD.
-    // It's not really helpful... Just call your factory
     return factory(root.angular);
   }
 }(this, function (angular) {
