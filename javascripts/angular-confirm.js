87522436b6e51d9e140fe77446e06b6db5ac2487
James Kleeh <james.kleeh@gmail.com>
2016-03-18 09:27:37 -0400

    Update to angular confirm 1.2.3 and ui-modal 0.14.3

--- javascripts/angular-confirm.js
+++ javascripts/angular-confirm.js
@@ -1,94 +1,108 @@
 /*
  * angular-confirm
- * http://schlogen.github.io/angular-confirm/
- * Version: 1.1.0 - 2015-14-07
- * License: Apache
+ * https://github.com/Schlogen/angular-confirm
+ * @version v1.2.3 - 2016-01-26
+ * @license Apache
  */
-angular.module('angular-confirm', ['ui.bootstrap'])
-  .controller('ConfirmModalController', ['$scope', '$modalInstance', 'data', function ($scope, $modalInstance, data) {
-    $scope.data = angular.copy(data);
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define(['angular'], factory);
+  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+    module.exports = factory(require('angular'));
+  } else {
+    return factory(root.angular);
+  }
+}(this, function (angular) {
+  angular.module('angular-confirm', ['ui.bootstrap.modal'])
+      .controller('ConfirmModalController', function ($scope, $uibModalInstance, data) {
+        $scope.data = angular.copy(data);
 
-    $scope.ok = function () {
-      $modalInstance.close();
-    };
+        $scope.ok = function (closeMessage) {
+          $uibModalInstance.close(closeMessage);
+        };
 
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
-    };
-
-  }])
-  .value('$confirmModalDefaults', {
-    template: '<div class="modal-header"><h3 class="modal-title">{{data.title}}</h3></div>' +
-    '<div class="modal-body">{{data.text}}</div>' +
-    '<div class="modal-footer">' +
-    '<button class="btn btn-primary" ng-click="ok()">{{data.ok}}</button>' +
-    '<button class="btn btn-default" ng-click="cancel()">{{data.cancel}}</button>' +
-    '</div>',
-    controller: 'ConfirmModalController',
-    defaultLabels: {
-      title: 'Confirm',
-      ok: 'OK',
-      cancel: 'Cancel'
-    }
-  })
-  .factory('$confirm', ['$modal', '$confirmModalDefaults', function ($modal, $confirmModalDefaults) {
-    return function (data, settings) {
-      settings = angular.extend($confirmModalDefaults, (settings || {}));
-
-      data = angular.extend({}, settings.defaultLabels, data || {});
-
-      if ('templateUrl' in settings && 'template' in settings) {
-        delete settings.template;
-      }
-
-      settings.resolve = {
-        data: function () {
-          return data;
-        }
-      };
-
-      return $modal.open(settings).result;
-    };
-  }])
-  .directive('confirm', ['$confirm', function ($confirm) {
-    return {
-      priority: 1,
-      restrict: 'A',
-      scope: {
-        confirmIf: "=",
-        ngClick: '&',
-        confirm: '@',
-        confirmSettings: "=",
-        confirmTitle: '@',
-        confirmOk: '@',
-        confirmCancel: '@'
-      },
-      link: function (scope, element, attrs) {
-
-
-        element.unbind("click").bind("click", function ($event) {
-
-          $event.preventDefault();
-
-          if (angular.isUndefined(scope.confirmIf) || scope.confirmIf) {
-
-            var data = {text: scope.confirm};
-            if (scope.confirmTitle) {
-              data.title = scope.confirmTitle;
-            }
-            if (scope.confirmOk) {
-              data.ok = scope.confirmOk;
-            }
-            if (scope.confirmCancel) {
-              data.cancel = scope.confirmCancel;
-            }
-            $confirm(data, scope.confirmSettings || {}).then(scope.ngClick);
-          } else {
-
-            scope.$apply(scope.ngClick);
+        $scope.cancel = function (dismissMessage) {
+          if (angular.isUndefined(dismissMessage)) {
+            dismissMessage = 'cancel';
           }
-        });
+          $uibModalInstance.dismiss(dismissMessage);
+        };
 
-      }
-    }
-  }]);
+      })
+      .value('$confirmModalDefaults', {
+        template: '<div class="modal-header"><h3 class="modal-title">{{data.title}}</h3></div>' +
+        '<div class="modal-body">{{data.text}}</div>' +
+        '<div class="modal-footer">' +
+        '<button class="btn btn-primary" ng-click="ok()">{{data.ok}}</button>' +
+        '<button class="btn btn-default" ng-click="cancel()">{{data.cancel}}</button>' +
+        '</div>',
+        controller: 'ConfirmModalController',
+        defaultLabels: {
+          title: 'Confirm',
+          ok: 'OK',
+          cancel: 'Cancel'
+        }
+      })
+      .factory('$confirm', function ($uibModal, $confirmModalDefaults) {
+        return function (data, settings) {
+          var defaults = angular.copy($confirmModalDefaults);
+          settings = angular.extend(defaults, (settings || {}));
+
+          data = angular.extend({}, settings.defaultLabels, data || {});
+
+          if ('templateUrl' in settings && 'template' in settings) {
+            delete settings.template;
+          }
+
+          settings.resolve = {
+            data: function () {
+              return data;
+            }
+          };
+
+          return $uibModal.open(settings).result;
+        };
+      })
+      .directive('confirm', function ($confirm) {
+        return {
+          priority: 1,
+          restrict: 'A',
+          scope: {
+            confirmIf: "=",
+            ngClick: '&',
+            confirm: '@',
+            confirmSettings: "=",
+            confirmTitle: '@',
+            confirmOk: '@',
+            confirmCancel: '@'
+          },
+          link: function (scope, element, attrs) {
+
+            element.unbind("click").bind("click", function ($event) {
+
+              $event.preventDefault();
+
+              if (angular.isUndefined(scope.confirmIf) || scope.confirmIf) {
+
+                var data = {text: scope.confirm};
+                if (scope.confirmTitle) {
+                  data.title = scope.confirmTitle;
+                }
+                if (scope.confirmOk) {
+                  data.ok = scope.confirmOk;
+                }
+                if (scope.confirmCancel) {
+                  data.cancel = scope.confirmCancel;
+                }
+                $confirm(data, scope.confirmSettings || {}).then(scope.ngClick);
+              } else {
+
+                scope.$apply(scope.ngClick);
+              }
+            });
+
+          }
+        }
+      });
+}));
